02ac8ca4363c36fc980d2fdf51dfa0b60f556acb
Florian Henryk Creutzig <florian.creutzig@nrg-u.no>
2021-08-25 11:57:50 +0200

    Added level manager skeleton

--- LevelManager.py
+++ LevelManager.py
@@ -1,1 +1,13 @@
+#The level manager, containing class of levels, functions for loading levels
+import pygame as pg
+
+def loadLevel(path):
+    pass
+
+class Level():
+    def __init__(self, enemies, platforms, projectiles, screenOffSet = [0, 0])
+        self.enemies = enemies
+        self.platforms = platforms
+        self.projectiles = projectiles
+        self.screenOffSet = screenOffSet
 
