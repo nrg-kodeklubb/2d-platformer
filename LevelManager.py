5f6c96a04db153276ccfe24d8f6f2e7f8d603573
haukkagu <haukkagu@gmail.com>
2021-08-27 18:23:37 +0200

    Implemented Level class with draw function

--- LevelManager.py
+++ LevelManager.py
@@ -4,10 +4,17 @@ import pygame as pg
 def loadLevel(path):
     pass
 
+block_size = 16
 class Level():
-    def __init__(self, enemies, platforms, projectiles, screenOffSet = [0, 0])
+    def __init__(self, tiles, enemies):
+        self.tiles = tiles
         self.enemies = enemies
-        self.platforms = platforms
-        self.projectiles = projectiles
-        self.screenOffSet = screenOffSet
 
+        self.width = len(self.tiles)
+        self.height = len(self.tiles[0])
+
+    def draw(self, screen):
+        for y in range(len(self.tiles)):
+            for x in range(len(self.tiles[0])):
+                if self.tiles[y][x] == 1:
+                    pg.draw.rect(screen, (255, 255, 255), (x*block_size, y*block_size, block_size, block_size))
